e929f8681bb40096520ee496d7d63548cf08c50c
fobic <sabychakraborty08@gmail.com>
2024-07-18 17:27:33 +0530

    optim(setup.py) : minor changes to the setup.py file.

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-# setup.py
-
 from setuptools import setup, find_packages
 
 setup(
     name='da_vis',
     version='0.1',
     description='A package for visualizing data and machine learning model performance',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='Sabyasachi',
     author_email='sabychakraborty08@gmail.com',
+    url='https://github.com/mixter3011/da_vis.git',  # Replace with your actual GitHub repo URL
     packages=find_packages(),
     install_requires=[
         'matplotlib',
@@ -25,4 +26,3 @@ setup(
     ],
     python_requires='>=3.6',
 )
-
